b2c553105ae7687fb6cfb7a230b91c3ef73f0821
Benny Neugebauer <bn@bennyn.de>
2015-10-14 15:09:10 +0200

    Moved scripts to "src/js"

---
+++ src/js/de/bennyn/crypto/ChaCha20/InsufficientArgumentsError.js
@@ -0,0 +1,12 @@
+/* global Error */
+
+de.bennyn.crypto.ChaCha20.InsufficientArgumentsError = (function() {
+  function Class(message) {
+    this.name = 'InsufficientArgumentsError';
+    this.message = message || 'Insufficient arguments in function call.';
+  }
+
+  Class.prototype = Error.prototype;
+
+  return Class;
+})();

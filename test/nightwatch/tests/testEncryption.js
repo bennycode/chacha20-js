fc9da1f22929c6366b440cb92a91cb09fd0d634f
Benny Neugebauer <bn@bennyn.de>
2015-10-15 18:03:49 +0200

    Renamed end-to-end test

--- test/nightwatch/tests/testEncryption.js
+++ test/nightwatch/tests/testEncryption.js
@@ -1,11 +1,11 @@
 module.exports = {
-  'Enter login credentials': function (browser) {
+  'Enter login credentials': function(browser) {
     var data = browser.globals;
     browser
       .url(browser.launch_url)
       .setValue("//*[@data-uie-name='enter-email']", data.email)
   },
-  'Submit login credentials': function (browser) {
+  'Submit login credentials': function(browser) {
     browser
       .click("//*[@data-uie-name='do-sign-in']")
       .end();

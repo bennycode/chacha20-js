9ed53b9948cffd21e92b15008aab53ac97d8220b
Benny Neugebauer <bn@bennyn.de>
2015-10-19 17:01:59 +0200

    Added linter to default Grunt task

--- conf/grunt/tasks/default.js
+++ conf/grunt/tasks/default.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.registerTask('default', 'Test app, transpile code and run development environment', function() {
     grunt.task.run([
+      'eslint:src',
       'test:phantom:false',
       'dist',
       'sass',

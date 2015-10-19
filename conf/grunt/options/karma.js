16af5d4814f7ec389892f7b7f2aa2ba02117ee43
Benny Neugebauer <bn@bennyn.de>
2015-10-19 15:41:18 +0200

    Using Karma.js for headless unit tests, resolves bennyn/chacha20-js#4

--- conf/grunt/options/karma.js
+++ conf/grunt/options/karma.js
@@ -1,21 +1,24 @@
 module.exports = {
-  jasmine: {
-    options: {
-      basePath: '',
-      frameworks: ['jasmine'],
-      files: [
-        '<%= dir.external_libraries %>/**/*.js',
-        '<%= dir.distributables %>/<%= pkg.name %>.min.js',
-        '<%= dir.jasmine.test_specifications %>/**/*Spec.js'
-      ],
-      exclude: [],
-      preprocessors: {},
-      reporters: ['progress'],
-      port: '<%= server.port.karma_test_runner %>',
-      colors: true,
-      autoWatch: true,
-      browsers: ['Chrome'],
-      singleRun: true
-    }
+  options: {
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      '<%= dir.external_libraries %>/**/*.js',
+      '<%= dir.distributables %>/<%= pkg.name %>.min.js',
+      '<%= dir.jasmine.test_specifications %>/**/*Spec.js'
+    ],
+    exclude: [],
+    preprocessors: {},
+    reporters: ['progress'],
+    port: '<%= server.port.karma_test_runner %>',
+    colors: true,
+    autoWatch: true,
+    singleRun: true
+  },
+  chrome: {
+    browsers: ['Chrome']
+  },
+  phantom: {
+    browsers: ['PhantomJS']
   }
 };

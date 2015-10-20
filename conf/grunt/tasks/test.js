c8092f2f126b4cb874ce8d086bb6009a551215fa
Benny Neugebauer <bn@bennyn.de>
2015-10-20 16:40:08 +0200

    Added default value for test engine

--- conf/grunt/tasks/test.js
+++ conf/grunt/tasks/test.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
   grunt.registerTask('test', 'Run unit tests', function(engine, create_coverage) {
+    (engine !== undefined) ? engine = engine : engine = 'phantom';
     (create_coverage !== undefined) ? create_coverage = create_coverage : create_coverage = false;
+    
     grunt.log.writeln('Run tests with engine: ' + engine);
     grunt.log.writeln('Create code coverage: ' + create_coverage);
 

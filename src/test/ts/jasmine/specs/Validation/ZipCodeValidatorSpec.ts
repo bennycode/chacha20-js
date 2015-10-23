f6cefe9b5edc78231e5ba2fe331f001a47da2b54
Benny Neugebauer <bn@bennyn.de>
2015-10-24 00:15:20 +0200

    Added namespace to resolve the "ZipCodeValidator"

--- src/test/ts/jasmine/specs/Validation/ZipCodeValidatorSpec.ts
+++ src/test/ts/jasmine/specs/Validation/ZipCodeValidatorSpec.ts
@@ -1,6 +1,6 @@
 describe('Zip code validation', () => {
   it('recognizes a valid zip code', function () {
-    var validator = new ZipCodeValidator();
+    var validator = new Validation.ZipCodeValidator();
     var actual = validator.isAcceptable('13088');
     expect(actual).toBe(true);
   });

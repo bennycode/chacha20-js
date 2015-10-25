f681cb4b5af040db2583f4f64019ed9454143c8b
Benny Neugebauer <bn@bennyn.de>
2015-10-25 17:21:37 +0100

    Removed uncommented tasks

--- conf/grunt/tasks/default.js
+++ conf/grunt/tasks/default.js
@@ -1,14 +1,5 @@
 module.exports = function(grunt) {
   grunt.registerTask('default', 'Test app, transpile code and run development environment', function() {
-    /**
-     grunt.task.run([
-     'eslint:src',
-     'test:phantom:false',
-     'dist',
-     'sass',
-     'dev'
-     ]);
-     **/
     grunt.task.run([
       'init',
       // Check

f0d5a04a570ed627f4dcde94b854a20952683e21
Benny Neugebauer <bn@bennyn.de>
2015-10-25 20:22:03 +0100

    Changed order of configuration properties

--- conf/grunt/options/jsdoc.js
+++ conf/grunt/options/jsdoc.js
@@ -1,11 +1,10 @@
 module.exports = {
   source_code: {
-    src: [
-      // TODO: Turn the source code into a global variable
-      '<%= dir.source_code.js %>/**/*.js'
-    ],
     options: {
       destination: '<%= dir.documentation %>'
-    }
+    },
+    src: [
+      '<%= dir.source_code.js %>/**/*.js'
+    ]
   }
 };
